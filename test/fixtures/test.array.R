20d394663faac42d3494b84e0cbdcf1feaab8111
Planeshifter <pburckhardt@outlook.com>
2015-08-01 21:51:54 +0200

    [INIT]

---
+++ test/fixtures/test.array.R
@@ -0,0 +1,21 @@
+options( digits = 16 )
+library( jsonlite )
+
+
+a = 0
+b = 1
+c = 0.5
+x = seq( -1000, 1000, 0.5 )
+y = dtriangle( x, a,b,c )
+
+cat( y, sep = ",\n" )
+
+data = list(
+	a = a,
+	b = b,
+	c = c,
+	data = x,
+	expected = y
+)
+
+write( toJSON( data, digits = 16, auto_unbox = TRUE ), "./test/fixtures/array.json" )
---
+++ test/fixtures/test.matrix.R
@@ -0,0 +1,21 @@
+options( digits = 16 )
+library( jsonlite )
+
+a = 0
+b = 1
+c = 0.5
+x = 0:24
+y = dtriangle( x, a,b,c )
+
+cat( y, sep = ",\n" )
+
+data = list(
+	a = a,
+	b = b,
+	c = c,
+	data = x,
+	expected = y
+)
+
+
+write( toJSON( data, digits = 16, auto_unbox = TRUE ), "./test/fixtures/matrix.json" )
